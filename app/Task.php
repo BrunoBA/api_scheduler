430c0dc95416eb435ffcc19d7eadfaff5f7c65d7
Bruno Barros <brunobarros@MBP-de-Bruno.lan>
2019-07-14 21:34:28 +0100

    Tests for Tasks

--- app/Task.php
+++ app/Task.php
@@ -7,4 +7,26 @@ use Illuminate\Database\Eloquent\Model;
 class Task extends Model
 {
     protected $fillable = ['content', 'finished'];
+
+    public function isFinished (): bool {
+        return $this->finished;
+    }
+
+    public function belongsToUser ($userId): bool {
+        return $this->user_id == $userId;
+    }
+
+    public function finish (): self {
+        $this->finished = true;
+        $this->save();
+
+        return $this;
+    }
+
+    public function remove (): bool {
+        if(!$this->isFinished()) {
+            return $this->delete();
+        }
+        return false;
+    }
 }
--- tests/Unit/TaskTest.php
+++ tests/Unit/TaskTest.php
@@ -5,20 +5,21 @@ namespace Tests\Unit;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use App\Task;
+use App\User;
 
 class TaskTest extends TestCase
 {
+    // use RefreshDatabase;
+    use DatabaseTransactions;
 
-    /**
-     * Check if the taskt can be checked with finish.
-     *
-     * @return void
-     */
-    public function testIfTheTaskCanBeFinished(): void
-    {
-        echo count(Task::all());
-        self::assertTrue(true);
+    private $task;
+    private const USER_ID = 1;
+
+    protected function setUp ():void  {
+        parent::setUp();
+        $this->task = factory(Task::class)->create(['user_id' => self::USER_ID]);
     }
 
     /**
@@ -28,6 +29,29 @@ class TaskTest extends TestCase
      */
     public function testIfTheTaskBelongsToUser(): void
     {
-        self::assertTrue(true);
+        $user = factory(User::class)->make(['id' => self::USER_ID]);
+        self::assertTrue(true, $this->task->belongsToUser($user->id));
+    }
+
+    /**
+     * Check if the task can be checked with finish.
+     *
+     * @return void
+     */
+    public function testIfTheTaskCanBeFinished(): void
+    {
+        $this->task = factory(Task::class)->create(['user_id' => self::USER_ID]);
+        $this->task->finish()->save();
+        self::assertTrue($this->task->isFinished());
+    }
+
+    /**
+     * Check if the task can't be remove.
+     *
+     * @return void
+     */
+    public function testIfTheTaskCantBeDeleted(): void
+    {
+        self::assertFalse($this->task->remove());
     }
 }
